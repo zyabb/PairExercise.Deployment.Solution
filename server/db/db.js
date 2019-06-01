bc1fad5e610841cd2b7305c566b971f5a5053e27
Ruslan Zyabbarov <rupam71@gmail.com>
2019-06-01 13:08:28 -0400

    We use Travis now

--- server/db/db.js
+++ server/db/db.js
@@ -1,13 +1,16 @@
-const Sequelize = require('sequelize')
-const pkg = require('../../package.json')
-const dbName = process.env.NODE_ENV === 'test' ? `${pkg.name}-test` : pkg.name
-const dbUrl = process.env.DATABASE_URL || `postgres://localhost:5432/${dbName}`
-const client = new Sequelize(dbUrl, { logging: false, operatorsAliases: false })
+const Sequelize = require('sequelize');
+const pkg = require('../../package.json');
+const dbName = process.env.NODE_ENV === 'test' ? `${pkg.name}-test` : pkg.name;
+const dbUrl = process.env.DATABASE_URL || `postgres://localhost:5432/${dbName}`;
+const client = new Sequelize(dbUrl, {
+  logging: false,
+  operatorsAliases: false,
+});
 
-module.exports = client
+module.exports = client;
 
 // This is a global Mocha hook used for resource cleanup.
 // Otherwise, Mocha v4+ does not exit after tests.
 if (process.env.NODE_ENV === 'test') {
-  after('close database connection', () => client.close())
+  after('close database connection', () => client.close());
 }
